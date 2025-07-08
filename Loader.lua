fffa3b30bc5cd355479dcf73de967696c5a5952d
water.5202 <yeahidkorb@gmail.com>
2025-07-08 22:34:26 +0100

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,7 +3,7 @@
 88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
 88      88    88    88            odD'      88      88    88 88ooo88 
 88  ooo 88    88    88          .88'        88      88    88 88~~~88 
-88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88    @uniquadev
+88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88    @uniquadev -- credits to this cool guy (creator of backdoor.exe)
  Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER 
 ]=]
 
